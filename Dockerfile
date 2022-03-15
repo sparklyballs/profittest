ee9674c672b71d826b2012fb33894fdb217e6bce
sparklyballs <sparklyballs@gmail.com>
2022-03-15 08:24:28 +0000

    first save up

---
+++ Dockerfile
@@ -0,0 +1,91 @@
+FROM python:3.9 AS profit_build
+
+# build arguments
+ARG DEBIAN_FRONTEND=noninteractive 
+ARG RELEASE
+
+# install build dependencies
+RUN \
+	apt-get update \
+	&& apt-get install \
+	--no-install-recommends -y \
+		ca-certificates \
+		curl \
+		jq \
+		lsb-release \
+		sudo
+
+# set workdir
+WORKDIR /profit-blockchain
+
+# fetch source
+RUN \
+	if [ -z ${RELEASE+x} ]; then \
+	RELEASE=$(curl -u "${SECRETUSER}:${SECRETPASS}" -sX GET "https://api.github.com/repos/ProfitCrypto/profit-blockchain/releases/latest" \
+	| jq -r ".tag_name"); \
+	fi \
+	&& git clone --branch ${RELEASE} --recurse-submodules=mozilla-ca https://github.com/ProfitCrypto/profit-blockchain.git . \
+	&& /bin/sh ./install.sh
+
+FROM python:3.9-slim
+
+# build arguments
+ARG DEBIAN_FRONTEND=noninteractive
+
+# environment variables
+ENV \
+        PROFIT_ROOT=/root/.profit/mainnet \
+        farmer_address= \
+        farmer_port= \
+        keys="generate" \
+        log_level="INFO" \
+        log_to_file="true" \
+        outbound_peer_count="20" \
+        peer_count="20" \
+        plots_dir="/plots" \
+        service="farmer" \
+        testnet="false" \
+        TZ="UTC" \
+        upnp="true"
+
+# legacy options
+ENV \
+	farmer="false" \
+	harvester="false"
+
+# set workdir
+WORKDIR /profit-blockchain
+
+# install dependencies
+RUN \
+	apt-get update \
+	&& apt-get install \
+	--no-install-recommends -y \
+		tzdata \
+	\
+# set timezone
+	\
+	&& ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime \
+	&& echo "$TZ" > /etc/timezone \
+	&& dpkg-reconfigure -f noninteractive tzdata \
+	\
+# cleanup
+	\
+	&& rm -rf \
+		/tmp/* \
+		/var/lib/apt/lists/* \
+		/var/tmp/*
+
+# set additional runtime environment variables
+ENV \
+	PATH=/profit-blockchain/venv/bin:$PATH
+
+# copy build files
+COPY --from=profit_build /profit-blockchain /profit-blockchain
+
+# copy local files
+COPY docker-*.sh /usr/local/bin/
+RUN chmod +x /usr/local/bin/docker-*.sh
+
+ENTRYPOINT ["docker-entrypoint.sh"]
+CMD ["docker-start.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2154
+if [[ -n "${TZ}" ]]; then
+  echo "Setting timezone to ${TZ}"
+  ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" > /etc/timezone
+fi
+
+cd /profit-blockchain || exit 1
+
+# shellcheck disable=SC1091
+. ./activate
+
+profit init --fix-ssl-permissions
+
+if [[ ${testnet} == 'true' ]]; then
+  echo "configure testnet"
+  profit configure --testnet true
+fi
+
+if [[ ${keys} == "persistent" ]]; then
+  echo "Not touching key directories"
+elif [[ ${keys} == "generate" ]]; then
+  echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
+  profit keys generate
+elif [[ ${keys} == "copy" ]]; then
+  if [[ -z ${ca} ]]; then
+    echo "A path to a copy of the farmer peer's ssl/ca required."
+    exit
+  else
+  profit init -c "${ca}"
+  fi
+else
+  profit keys add -f "${keys}"
+fi
+
+for p in ${plots_dir//:/ }; do
+  mkdir -p "${p}"
+  if [[ ! $(ls -A "$p") ]]; then
+    echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
+  fi
+  profit plots add -d "${p}"
+done
+
+profit configure --upnp "${upnp}"
+
+if [[ -n "${log_level}" ]]; then
+  profit configure --log-level "${log_level}"
+fi
+
+if [[ -n "${peer_count}" ]]; then
+  profit configure --set-peer-count "${peer_count}"
+fi
+
+if [[ -n "${outbound_peer_count}" ]]; then
+  profit configure --set_outbound-peer-count "${outbound_peer_count}"
+fi
+
+if [[ -n ${farmer_address} && -n ${farmer_port} ]]; then
+  profit configure --set-farmer-peer "${farmer_address}:${farmer_port}"
+fi
+
+sed -i 's/localhost/127.0.0.1/g' "$PROFIT_ROOT/config/config.yaml"
+
+if [[ ${log_to_file} != 'true' ]]; then
+  sed -i 's/log_stdout: false/log_stdout: true/g' "$PROFIT_ROOT/config/config.yaml"
+else
+  sed -i 's/log_stdout: true/log_stdout: false/g' "$PROFIT_ROOT/config/config.yaml"
+fi
+
+# Map deprecated legacy startup options.
+if [[ ${farmer} == "true" ]]; then
+  service="farmer-only"
+elif [[ ${harvester} == "true" ]]; then
+  service="harvester"
+fi
+
+if [[ ${service} == "harvester" ]]; then
+  if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
+    echo "A farmer peer address, port, and ca path are required."
+    exit
+  fi
+fi
+
+exec "$@"
